a6f7a814334753c66b02fb784d1123ef5dc35d78
leo <chichali374@gmail.com>
2022-09-12 17:17:15 +0800

    新增產品頁面及scss檔案

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,3 +3,14 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 });
+
+$(function() {
+  $("#tabs-nav a").click(function(event) {
+      event.preventDefault();
+      $("#tabs-nav a").removeClass("tabs-menu-active");
+      $(this).addClass("tabs-menu-active");
+      $(".tabs-panel").removeClass("animated-tabs bounceIn").hide();
+      var tab_id = $(this).data("target");
+      $("#"+tab_id).addClass("animated-tabs bounceIn").show();
+  });
+});
