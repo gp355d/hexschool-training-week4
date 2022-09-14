e448e5601e0518c9cf24b7f9a125d0e53c4660c1
leo <chichali374@gmail.com>
2022-09-14 22:16:39 +0800

    Update 2022-09-14T14:16:37.170Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,4 +4,14 @@ console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
 });
+$(function () {
+  $("#tabs-nav a").click(function (event) {
+    event.preventDefault();
+    $("#tabs-nav a").removeClass("tabs-menu-active");
+    $(this).addClass("tabs-menu-active");
+    $(".tabs-panel").removeClass("animated-tabs bounceIn").hide();
+    var tab_id = $(this).data("target");
+    $("#" + tab_id).addClass("animated-tabs bounceIn").show();
+  });
+});
 //# sourceMappingURL=all.js.map
